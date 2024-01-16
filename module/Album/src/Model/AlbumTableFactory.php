75001003ce00a17d5920f9c146c6c249ca6682e0
Joey Smith <jsmith@webinertia.net>
2024-01-16 16:39:03 -0600

    Update AlbumTableFactory.php

---
+++ module/Album/src/Model/AlbumTableFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Album\Model;
+
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\Resultset\Resultset;
+use Laminas\Db\TableGateway\TableGateway;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+
+final class AlbumTableFactory implements FactoryInterface
+{
+    /** @inheritDoc*/
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
+    {
+        return $requestedName(
+                  'album',
+        $container->get(AdapterInterface::class),
+        null,
+        new ResultSet(new Album()),
+        null
+        );
+    }
+}
